b07c970d6debf7317720ee7d8958a916b8c6bf62
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2022-10-14 19:44:12 -0700

    Add files via upload

---
+++ img/Player_Car_W.asm
@@ -0,0 +1,7 @@
+Player_Car_W:
+	.byte	8,5
+	.byte	%00000000
+	.byte	%00001111
+	.byte	%00111001
+	.byte	%00001111
+	.byte	%00000000
---
+++ img/Puddle_Splash.asm
@@ -0,0 +1,10 @@
+Puddle_Splash:
+	.byte	8,8
+	.byte	%00001100
+	.byte	%00011000
+	.byte	%00001110
+	.byte	%00000001
+	.byte	%00000000
+	.byte	%00111000
+	.byte	%01100000
+	.byte	%00110100
